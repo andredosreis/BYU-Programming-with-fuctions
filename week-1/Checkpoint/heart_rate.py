b714b52b66e9bc962fc3b258c91bc15c36da13f7
andredosreis <63263157+andredosreis@users.noreply.github.com>
2021-09-14 11:37:35 +0100

    Conclusion exercise 1

---
+++ week-1/Checkpoint/heart_rate.py
@@ -0,0 +1,7 @@
+n1 = int(input('Please enter your age: '))
+
+rage = 220 - n1
+rate1 = rage * 0.65
+rate2 = rage * 0.85
+
+print(f'When your exercise to stregthen your heart, you should keep your heart rate between {rate1:.0f} and {rate2:.0f} beats per minute')
