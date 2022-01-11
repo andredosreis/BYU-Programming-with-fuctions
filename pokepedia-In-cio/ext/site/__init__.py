bfdeb410bc7f0f4ccff16db3667df578c23139ce
andredosreis <63263157+andredosreis@users.noreply.github.com>
2022-01-11 21:45:17 +0000

    finish project

--- pokepedia-In-cio/ext/site/__init__.py
+++
@@ -1,4 +0,0 @@
-from .routes import bp
-
-def init_app(app):
-    app.register_blueprint(bp)
--- pokepedia-In-cio/ext/site/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('routes', __name__)
-
-@bp.route("/")
-def home():
-    return render_template('index.html')
